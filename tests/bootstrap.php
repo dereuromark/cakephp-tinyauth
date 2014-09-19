ab874a96ca7cdbfd232eed270b3fb3c9156d2211
euromark <euromark@web.de>
2014-09-19 14:07:14 +0200

    use default ds

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -62,7 +62,7 @@ if (!getenv('db_class')) {
 	putenv('db_dsn=sqlite::memory:');
 }
 
-Cake\Datasource\ConnectionManager::config('test', [
+Cake\Datasource\ConnectionManager::config('default', [
 	'className' => 'Cake\Database\Connection',
 	'driver' => getenv('db_class'),
 	'dsn' => getenv('db_dsn'),
