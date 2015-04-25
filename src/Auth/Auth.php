38f3195a6eb442ca1ce28800cacae0a1aac45321
Mark Scherer <euromark@web.de>
2015-04-25 12:31:50 +0200

    Remove untested and unworking code

--- src/Auth/Auth.php
+++ src/Auth/Auth.php
@@ -12,44 +12,19 @@ use App\Controller\Component\AuthComponent;
 use Cake\Utility\Hash;
 
 /**
- * Convenience wrapper to access Auth data and check on rights/roles.
+ * Convenience wrapper to access Auth data and check on roles.
  *
- * It can be used anywhere in the application due to static access.
- * So in the view we can use this shortcut to check if a user is logged in:
- *
- *   if (Auth::id()) {
- *     // Display element
- *   }
- *
- * Simply add it at the end of your bootstrap file (after the plugin is loaded):
- *
- *   use Tools\Auth;
+ *   use TinyAuth\Auth\Auth;
  *
  * Expects the Role session infos to be either
  * 	- `Auth.User.role_id` (single) or
  * 	- `Auth.User.Role` (multi - flat array of roles, or array role data)
  * and can be adjusted via constants and defined().
- * Same goes for Right data.
- *
- * Note: This uses AuthComponent internally to work with both stateful and stateless auth.
  *
  * @author Mark Scherer
  * @license MIT
- * @php 5
  */
 class Auth {
-
-	/**
-	 * Get the user id of the current session.
-	 *
-	 * This can be used anywhere to check if a user is logged in.
-	 *
-	 * @return mixed User id if existent, null otherwise.
-	 * @deprecated Not usable in CakePHP 3.
-	 */
-	public static function id() {
-		return AuthComponent::user('id');
-	}
 
 	/**
 	 * Normalizes roles.
@@ -71,20 +46,9 @@ class Auth {
 	}
 
 	/**
-	 * Get the user data of the current session.
-	 *
-	 * @param string $key Key in dot syntax.
-	 * @return mixed Data
-	 * @deprecated Not usable in CakePHP 3.
-	 */
-	public static function user($key = null) {
-		return AuthComponent::user($key);
-	}
-
-	/**
 	 * Check if the current session has this role.
 	 *
-	 * @param mixed $role
+	 * @param mixed $expectedRole
 	 * @param array $currentRoles
 	 * @return bool Success
 	 */
@@ -99,7 +63,7 @@ class Auth {
 	 * You can either require one of the roles (default), or you can require all
 	 * roles to match.
 	 *
-	 * @param mixed $roles
+	 * @param mixed $expectedRoles
 	 * @param bool $oneRoleIsEnough (if all $roles have to match instead of just one)
 	 * @param array $currentRoles
 	 * @return bool Success
@@ -124,26 +88,6 @@ class Auth {
 		}
 
 		if ($count === count($expectedRoles)) {
-			return true;
-		}
-		return false;
-	}
-
-	/**
-	 * Check if the current session has this right.
-	 *
-	 * Rights can be an additional element to give permissions, e.g.
-	 * the right to send messages/emails, to friend request other users,...
-	 * This can be set via Right model and stored in the Auth array upon login
-	 * the same way the roles are.
-	 *
-	 * @param mixed $role
-	 * @param array $currentRights
-	 * @return bool Success
-	 */
-	public static function hasRight($expectedRight, $currentRights) {
-		$currentRights = (array)$currentRights;
-		if (array_key_exists($expectedRight, $currentRights) && !empty($currentRights[$expectedRight])) {
 			return true;
 		}
 		return false;
