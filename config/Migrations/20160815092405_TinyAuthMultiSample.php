58c0a0915a9a6ed00d80fa8d1d0bdd8ca5e70dc0
Lubos Remplik <lubos@on-idle.com>
2016-08-15 11:26:23 +0200

    Migrations multiple sample

---
+++ config/Migrations/20160815092405_TinyAuthMultiSample.php
@@ -0,0 +1,80 @@
+<?php
+use Migrations\AbstractMigration;
+
+class TinyAuthMultiSample extends AbstractMigration
+{
+    public function up()
+    {
+
+        $this->table('roles')
+            ->addColumn('name', 'string', [
+                'default' => null,
+                'limit' => 64,
+                'null' => false,
+            ])
+            ->addColumn('description', 'string', [
+                'default' => null,
+                'limit' => 255,
+                'null' => false,
+            ])
+            ->addColumn('alias', 'string', [
+                'default' => null,
+                'limit' => 20,
+                'null' => false,
+            ])
+            ->addColumn('created', 'datetime', [
+                'default' => null,
+                'limit' => null,
+                'null' => true,
+            ])
+            ->addColumn('modified', 'datetime', [
+                'default' => null,
+                'limit' => null,
+                'null' => true,
+            ])
+            ->create();
+
+        $this->table('users')
+            ->addColumn('username', 'string', [
+                'default' => null,
+                'limit' => 256,
+                'null' => true,
+            ])
+            ->addColumn('password', 'string', [
+                'default' => null,
+                'limit' => 256,
+                'null' => true,
+            ])
+            ->addColumn('created', 'timestamp', [
+                'default' => null,
+                'limit' => null,
+                'null' => true,
+            ])
+            ->addColumn('updated', 'timestamp', [
+                'default' => null,
+                'limit' => null,
+                'null' => true,
+            ])
+            ->create();
+
+        $this->table('users_roles')
+            ->addColumn('user_id', 'integer', [
+                'default' => null,
+                'limit' => 11,
+                'null' => true,
+            ])
+            ->addColumn('role_id', 'integer', [
+                'default' => null,
+                'limit' => 11,
+                'null' => true,
+            ])
+            ->create();
+    }
+
+    public function down()
+    {
+        $this->dropTable('roles');
+        $this->dropTable('users');
+        $this->dropTable('users_roles');
+    }
+}
