af2de6f259b0d82a4a0bb88a98870c43eeb8dd15
Ope Adeyomoye <victoradeyomoye@yahoo.co.uk>
2018-07-20 23:53:56 +0100

    Added an Acl adapter interface

---
+++ src/Auth/AclAdapter/AclAdapterInterface.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace TinyAuth\Auth\AclAdapter;
+
+interface AclAdapterInterface
+{
+    /**
+     * Generates and returns a TinyAuth access control list.
+     *
+     * @param array $availableRoles A list of available user roles.
+     * @param array $tinyConfig Current TinyAuth configuration values.
+     *
+     * @return array
+     */
+    public function getAcl($availableRoles, $tinyConfig);
+}
