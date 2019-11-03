13f1ad2e01368d5de7762b3c9fc5063d73d3e2c0
mscherer <mark.scherer@spryker.com>
2019-11-03 12:20:38 +0100

    Add adapter tests

---
+++ tests/TestCase/Auth/AclAdapter/IniAclAdapterTest.php
@@ -0,0 +1,42 @@
+<?php
+namespace TinyAuth\Test\Auth\AclAdapter;
+
+use Cake\Core\Plugin;
+use Cake\TestSuite\TestCase;
+use TinyAuth\Auth\AclAdapter\IniAclAdapter;
+
+class IniAclAdapterTest extends TestCase {
+
+	/**
+	 * @var \TinyAuth\Auth\AclAdapter\IniAclAdapter
+	 */
+	public $adapter;
+
+	/**
+	 * @return void
+	 */
+	public function setUp() {
+		parent::setUp();
+
+		$this->adapter = new IniAclAdapter();
+	}
+
+	/**
+	 * @return void
+	 */
+	public function testGetAcl() {
+		$availableRoles = [
+			'user' => 1,
+			'moderator' => 2,
+			'admin' => 3,
+		];
+		$config = [
+			'filePath' => Plugin::path('TinyAuth') . 'tests' . DS . 'test_files' . DS,
+			'file' => 'auth_acl.ini',
+		];
+		$result = $this->adapter->getAcl($availableRoles, $config);
+
+		$this->assertCount(14, $result);
+	}
+
+}
---
+++ tests/TestCase/Auth/AllowAdapter/IniAllowAdapterTest.php
@@ -0,0 +1,37 @@
+<?php
+namespace TinyAuth\Test\Auth\AllowAdapter;
+
+use Cake\Core\Plugin;
+use Cake\TestSuite\TestCase;
+use TinyAuth\Auth\AllowAdapter\IniAllowAdapter;
+
+class IniAllowAdapterTest extends TestCase {
+
+	/**
+	 * @var \TinyAuth\Auth\AllowAdapter\IniAllowAdapter
+	 */
+	public $adapter;
+
+	/**
+	 * @return void
+	 */
+	public function setUp() {
+		parent::setUp();
+
+		$this->adapter = new IniAllowAdapter();
+	}
+
+	/**
+	 * @return void
+	 */
+	public function testGetAllow() {
+		$config = [
+			'filePath' => Plugin::path('TinyAuth') . 'tests' . DS . 'test_files' . DS,
+			'file' => 'auth_allow.ini',
+		];
+		$result = $this->adapter->getAllow($config);
+
+		$this->assertCount(3, $result);
+	}
+
+}
