52c7557c25668becc9254da1a507af84e39b52a4
mscherer <mark.scherer@spryker.com>
2021-11-12 11:26:00 +0100

    Fix up routes file.

--- tests/config/routes.php
+++ tests/config/routes.php
@@ -1,11 +1,10 @@
 <?php
 
-use Cake\Routing\Route\DashedRoute;
 use Cake\Routing\RouteBuilder;
 use Cake\Routing\Router;
 
 Router::reload();
 
 Router::scope('/', function(RouteBuilder $routes) {
-	$routes->fallbacks(DashedRoute::class);
+	$routes->fallbacks();
 });
