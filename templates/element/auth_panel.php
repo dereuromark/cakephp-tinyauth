8eeb9549ef541f0206890b9328b6ec6be65111cc
mscherer <dereuromark@web.de>
2025-04-24 16:03:48 +0200

    Rearrange panel

--- templates/element/auth_panel.php
+++ templates/element/auth_panel.php
@@ -33,86 +33,97 @@ if (!isset($isPublic)) {
 <section class="section-tile">
 	<h1>TinyAuth</h1>
 
-	<h2>Current URL</h2>
-	<?php
-	echo Debugger::exportVar($params);
-	?>
-	<br/>
-	<p>TinyAuth URL path: <b style="font-weight: bold"><?php echo h($path); ?></b></p>
+	<table>
+		<tr>
+			<td>
 
-	<h2>Authentication (allow)</h2>
-	<p>
-	<?php
-	if (Config::get('allowAdapter')) {
-		if ($isPublic === null) {
-			echo '<div><small>No information available</small></div>';
-		} else {
-			$icon = $isPublic ? AuthPanel::ICON_PUBLIC : AuthPanel::ICON_RESTRICTED;
-			echo '<p>' . $icon . ' <b style="font-weight: bold">' . ($isPublic ? 'public' : 'secured') . '</b> action</p>';
-			if ($isPublic) {
-				echo '<div><small>Any guest can visit this page</small></div>';
-			} else {
-				echo '<div><small>Login required to visit this page</small></div>';
-			}
-		}
-	} else {
-		echo '<i>disabled</i>';
-	}
-	?>
-	</p>
+				<h2>Current URL</h2>
+				<?php
+				echo Debugger::exportVar($params);
+				?>
+				<br/>
+				<p>TinyAuth URL path: <b style="font-weight: bold"><?php echo h($path); ?></b></p>
 
-	<?php if ($authenticationProvider) {
-		echo '<p>';
-		echo 'Authentication provider used: ' . get_class($authenticationProvider);
-		echo '</p>';
-	} ?>
-	<?php if ($identificationProvider) {
-		echo '<p>';
-		echo 'Identification provider used: ' . get_class($identificationProvider);
-		echo '</p>';
-	} ?>
+				<h2>Authentication (allow)</h2>
+				<p>
+					<?php
+					if (Config::get('allowAdapter')) {
+						if ($isPublic === null) {
+							echo '<div><small>No information available</small></div>';
+						} else {
+							$icon = $isPublic ? AuthPanel::ICON_PUBLIC : AuthPanel::ICON_RESTRICTED;
+							echo '<p>' . $icon . ' <b style="font-weight: bold">' . ($isPublic ? 'public' : 'secured') . '</b> action</p>';
+							if ($isPublic) {
+								echo '<div><small>Any guest can visit this page</small></div>';
+							} else {
+								echo '<div><small>Login required to visit this page</small></div>';
+							}
+						}
+					} else {
+						echo '<i>disabled</i>';
+					}
+					?>
+				</p>
 
-	<h2>Authorization (ACL)</h2>
-	<?php
-	if (Config::get('aclAdapter')) {
-		if (!empty($user)) {
-			$primaryKey = $config['idColumn'];
+				<?php if ($authenticationProvider) {
+					echo '<p>';
+					echo 'Authentication provider used: ' . get_class($authenticationProvider);
+					echo '</p>';
+				} ?>
+				<?php if ($identificationProvider) {
+					echo '<p>';
+					echo 'Identification provider used: ' . get_class($identificationProvider);
+					echo '</p>';
+				} ?>
 
-			echo '<p>Logged in with ID <b style="font-weight: bold">' . h($user[$primaryKey]) . '</b></p>';
+			</td>
+			<td>
 
-			echo '<h4>Roles</h4>';
-			Debugger::dump($roles);
+				<h2>Authorization (ACL)</h2>
+				<?php
+				if (Config::get('aclAdapter')) {
+					if (!empty($user)) {
+						$primaryKey = $config['idColumn'];
 
-		} else {
-			echo '<i>not logged in</i><br/>';
-		}
+						echo '<p>Logged in with ID <b style="font-weight: bold">' . h($user[$primaryKey]) . '</b></p>';
 
-	} else {
-		echo '<i>disabled</i><br/>';
-	}
-	?>
+						echo '<h4>Roles</h4>';
+						Debugger::dump($roles);
 
-	<br/>
+					} else {
+						echo '<i>not logged in</i><br/>';
+					}
 
-	<?php if (!empty($access)) { ?>
-		<p>
-			<?php if (!$isPublic) { ?>
-				The following roles have access to this action:
-			<?php } else { ?>
-				The following roles would have access to this action once you revoke public access:
-			<?php } ?>
-		</p>
-		<ul>
-			<?php
-			foreach ($availableRoles as $role => $id) {
-				echo '<li>';
-				echo ($access[$role] ? '<b style="font-weight: bold; color: green">&#10003;</b>' : '<b style="font-weight: bold; color: red">&#128683;</b>') . ' ';
-				echo h($role) . ' (id ' . $id . ')';
-				echo '</li>';
-			}
-			?>
-		</ul>
-	<?php } ?>
+				} else {
+					echo '<i>disabled</i><br/>';
+				}
+				?>
+
+				<br/>
+
+				<?php if (!empty($access)) { ?>
+					<p>
+						<?php if (!$isPublic) { ?>
+							The following roles have access to this action:
+						<?php } else { ?>
+							The following roles would have access to this action once you revoke public access:
+						<?php } ?>
+					</p>
+					<ul>
+						<?php
+						foreach ($availableRoles as $role => $id) {
+							echo '<li>';
+							echo ($access[$role] ? '<b style="font-weight: bold; color: green">&#10003;</b>' : '<b style="font-weight: bold; color: red">&#128683;</b>') . ' ';
+							echo h($role) . ' (id ' . $id . ')';
+							echo '</li>';
+						}
+						?>
+					</ul>
+				<?php } ?>
+
+			</td>
+		</tr>
+	</table>
 
 	<h2>Identity</h2>
 	<?php
